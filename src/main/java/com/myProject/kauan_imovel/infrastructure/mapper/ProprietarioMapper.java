92c361d860e67907664c7f40283d2c1284c541c4
Dev-Souza <kauan.souza.dev@gmail.com>
2025-08-26 10:38:56 -0300

    fix: pom.xml e mapper

--- src/main/java/com/myProject/kauan_imovel/infrastructure/mapper/ProprietarioMapper.java
+++ src/main/java/com/myProject/kauan_imovel/infrastructure/mapper/ProprietarioMapper.java
@@ -10,8 +10,6 @@ import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ProprietarioMapper {
-    ProprietarioMapper INSTANCE = Mappers.getMapper(ProprietarioMapper.class);
-
     PessoaProprietarioEntity toEntity(CadastrarPessoaProprietarioCommand command);
     List<PessoaProprietarioEntity> toEntities(List<CadastrarPessoaProprietarioCommand> commands);
 
