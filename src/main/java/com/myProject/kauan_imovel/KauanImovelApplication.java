85edbafe7dac68de9139c7acf678958c61f3a238
Dev-Souza <kauan.souza.dev@gmail.com>
2025-08-26 09:04:50 -0300

    feat: init project the imovel

---
+++ src/main/java/com/myProject/kauan_imovel/KauanImovelApplication.java
@@ -0,0 +1,13 @@
+package com.myProject.kauan_imovel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KauanImovelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KauanImovelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myProject/kauan_imovel/KauanImovelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myProject.kauan_imovel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KauanImovelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
