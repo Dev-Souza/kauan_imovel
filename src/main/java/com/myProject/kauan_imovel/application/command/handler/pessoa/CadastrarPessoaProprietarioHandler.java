285808dae261556487761e59d1844c9b40e12fb8
Dev-Souza <kauan.souza.dev@gmail.com>
2025-08-26 10:10:03 -0300

    feat: operando o handler de cadastrar pessoaProprietario

--- src/main/java/com/myProject/kauan_imovel/application/command/handler/pessoa/CadastrarPessoaProprietarioHandler.java
+++ src/main/java/com/myProject/kauan_imovel/application/command/handler/pessoa/CadastrarPessoaProprietarioHandler.java
@@ -1,8 +1,23 @@
 package com.myProject.kauan_imovel.application.command.handler.pessoa;
 
+import com.myProject.kauan_imovel.application.command.command.pessoa.CadastrarPessoaProprietarioCommand;
+import com.myProject.kauan_imovel.domain.pessoa.PessoaProprietarioEntity;
+import com.myProject.kauan_imovel.infrastructure.mapper.ProprietarioMapper;
+import com.myProject.kauan_imovel.infrastructure.repository.pessoa.PessoaProprietarioRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CadastrarPessoaProprietarioHandler {
+    private final PessoaProprietarioRepository repository;
+    private final ProprietarioMapper proprietarioMapper;
 
+    public CadastrarPessoaProprietarioHandler(PessoaProprietarioRepository repository, ProprietarioMapper mapper, ProprietarioMapper proprietarioMapper) {
+        this.repository = repository;
+        this.proprietarioMapper = proprietarioMapper;
+    }
+
+    public void handle(CadastrarPessoaProprietarioCommand command) {
+        PessoaProprietarioEntity proprietario = proprietarioMapper.toEntity(command);
+        repository.save(proprietario);
+    }
 }
