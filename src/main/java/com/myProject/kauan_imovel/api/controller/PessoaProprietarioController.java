4f1cc52c16bed85402e3fe2ef2db723cc461b368
Dev-Souza <kauan.souza.dev@gmail.com>
2025-08-28 09:48:25 -0300

    feat: function de alterar pessoa proprietaria

--- src/main/java/com/myProject/kauan_imovel/api/controller/PessoaProprietarioController.java
+++ src/main/java/com/myProject/kauan_imovel/api/controller/PessoaProprietarioController.java
@@ -2,6 +2,7 @@ package com.myProject.kauan_imovel.api.controller;
 
 import com.myProject.kauan_imovel.application.command.command.pessoa.CadastrarPessoaProprietarioCommand;
 import com.myProject.kauan_imovel.application.command.command.pessoa.DeletePessoaProprietariaCommand;
+import com.myProject.kauan_imovel.application.command.handler.pessoa.AlterarPessoaProprietariaHandler;
 import com.myProject.kauan_imovel.application.command.handler.pessoa.CadastrarPessoaProprietarioHandler;
 import com.myProject.kauan_imovel.application.command.handler.pessoa.DeletePessoaProprietariaHandler;
 import com.myProject.kauan_imovel.application.query.handler.BuscarTodasPessoasProprietariasHandler;
@@ -15,11 +16,13 @@ import java.util.List;
 @RequestMapping("/pessoa-proprietario")
 public class PessoaProprietarioController {
     private final CadastrarPessoaProprietarioHandler cadastrarPessoaProprietarioHandler;
+    private final AlterarPessoaProprietariaHandler alterarPessoaProprietariaHandler;
     private final BuscarTodasPessoasProprietariasHandler buscarTodasPessoasProprietariasHandler;
     private final DeletePessoaProprietariaHandler deletePessoaProprietariaHandler;
 
-    public PessoaProprietarioController(CadastrarPessoaProprietarioHandler cadastrarPessoaProprietarioHandler, DeletePessoaProprietariaHandler deletePessoaProprietariaHandler, BuscarTodasPessoasProprietariasHandler buscarTodasPessoasProprietariasHandler) {
+    public PessoaProprietarioController(CadastrarPessoaProprietarioHandler cadastrarPessoaProprietarioHandler, DeletePessoaProprietariaHandler deletePessoaProprietariaHandler, BuscarTodasPessoasProprietariasHandler buscarTodasPessoasProprietariasHandler,  AlterarPessoaProprietariaHandler alterarPessoaProprietariaHandler) {
         this.cadastrarPessoaProprietarioHandler = cadastrarPessoaProprietarioHandler;
+        this.alterarPessoaProprietariaHandler = alterarPessoaProprietariaHandler;
         this.deletePessoaProprietariaHandler = deletePessoaProprietariaHandler;
         this.buscarTodasPessoasProprietariasHandler = buscarTodasPessoasProprietariasHandler;
     }
@@ -34,6 +37,13 @@ public class PessoaProprietarioController {
     // BUSCAR TODAS AS PESSOAS PROPRIETÁRIAS
     @GetMapping("/listar")
     public ResponseEntity<List<PessoaProprietarioQueryDTO>> listar(){ return buscarTodasPessoasProprietariasHandler.handle(); }
+
+    // ALTERAR PESSOA PROPRIETÁRIA
+    @PutMapping("/alterar/{id}")
+    public ResponseEntity<Void> update(@PathVariable("id") Long id, @RequestBody CadastrarPessoaProprietarioCommand cmd){
+        alterarPessoaProprietariaHandler.handle(id, cmd);
+        return ResponseEntity.ok().build();
+    }
 
     // DELETAR PESSOA PROPRIETARIA
     @DeleteMapping("/deletar/{id}")
---
+++ src/main/java/com/myProject/kauan_imovel/application/command/handler/pessoa/AlterarPessoaProprietariaHandler.java
@@ -0,0 +1,29 @@
+package com.myProject.kauan_imovel.application.command.handler.pessoa;
+
+import com.myProject.kauan_imovel.application.command.command.pessoa.CadastrarPessoaProprietarioCommand;
+import com.myProject.kauan_imovel.domain.pessoa.PessoaProprietarioEntity;
+import com.myProject.kauan_imovel.infrastructure.repository.pessoa.PessoaProprietarioRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AlterarPessoaProprietariaHandler {
+
+    private final PessoaProprietarioRepository repository;
+
+    public AlterarPessoaProprietariaHandler(PessoaProprietarioRepository repository) {
+        this.repository = repository;
+    }
+
+    public void handle(Long id, CadastrarPessoaProprietarioCommand command) {
+        if(repository.existsById(id)){
+            PessoaProprietarioEntity entity = repository.findById(id).get();
+            entity.setNome(command.nome());
+            entity.setCpf(command.cpf());
+            entity.setEmail(command.email());
+            entity.setTelefone(command.telefone());
+            entity.setDataCadastro(command.dataCadastro());
+            entity.setObservacoes(command.observacoes());
+            repository.save(entity);
+        }
+    }
+}
