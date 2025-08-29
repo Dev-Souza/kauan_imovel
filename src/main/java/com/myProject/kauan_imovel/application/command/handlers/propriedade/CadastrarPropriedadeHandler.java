b5deb6dfa989c93286bfe3ff702ee6dcd4552f94
Dev-Souza <kauan.souza.dev@gmail.com>
2025-08-29 08:12:47 -0300

    feat: init do handle de cadastrar propriedade

---
+++ src/main/java/com/myProject/kauan_imovel/application/command/handlers/propriedade/CadastrarPropriedadeHandler.java
@@ -0,0 +1,9 @@
+package com.myProject.kauan_imovel.application.command.handlers.propriedade;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CadastrarPropriedadeHandler {
+
+
+}
