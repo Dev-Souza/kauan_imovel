272e476e87becc47802a12b8ce0fab9b85ccf390
Dev-Souza <kauan.souza.dev@gmail.com>
2025-09-01 08:55:55 -0300

    feat: mapper de comprador

---
+++ src/main/java/com/myProject/kauan_imovel/infrastructure/mapper/CompradorMapper.java
@@ -0,0 +1,43 @@
+package com.myProject.kauan_imovel.infrastructure.mapper;
+
+import com.myProject.kauan_imovel.application.command.pessoa.CadastrarPessoaCompradoraCommand;
+import com.myProject.kauan_imovel.domain.pessoa.PessoaCompradoraEntity;
+import com.myProject.kauan_imovel.domain.pessoa.dto.PessoaCompradoraQueryDTO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CompradorMapper {
+
+    public PessoaCompradoraEntity toEntity(CadastrarPessoaCompradoraCommand cmd){
+        if ( cmd == null ) {
+            return null;
+        }
+        PessoaCompradoraEntity entity = new PessoaCompradoraEntity();
+        entity.setNome(cmd.nome());
+        entity.setCpf(cmd.cpf());
+        entity.setEmail(cmd.email());
+        entity.setTelefone(cmd.telefone());
+        entity.setFaixaPrecoDesejada(cmd.faixaPrecoDesejada());
+        entity.setPossuiFinanciamentoAprovado(cmd.possuiFinanciamentoAprovado());
+        entity.setInstituicaoFinanceira(cmd.instituicaoFinanceira());
+        return entity;
+    }
+
+    public PessoaCompradoraQueryDTO toDTO(PessoaCompradoraEntity entity){
+        if ( entity == null ) {
+            return null;
+        }
+
+        return new PessoaCompradoraQueryDTO(
+                entity.getId(),
+                entity.getNome(),
+                entity.getCpf(),
+                entity.getEmail(),
+                entity.getTelefone(),
+                entity.getFaixaPrecoDesejada(),
+                entity.isPossuiFinanciamentoAprovado(),
+                entity.getInstituicaoFinanceira(),
+                entity.getCompras()
+        );
+    }
+}
