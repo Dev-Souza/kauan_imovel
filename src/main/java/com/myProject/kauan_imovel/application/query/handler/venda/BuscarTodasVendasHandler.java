a9fa3a7d6a1322c1a544272dd57fa3506c6b8ff8
Dev-Souza <kauan.souza.dev@gmail.com>
2025-09-01 11:02:38 -0300

    feat: buscar todas as vendas

---
+++ src/main/java/com/myProject/kauan_imovel/application/query/handler/venda/BuscarTodasVendasHandler.java
@@ -0,0 +1,23 @@
+package com.myProject.kauan_imovel.application.query.handler.venda;
+
+import com.myProject.kauan_imovel.domain.venda.dto.VendaQueryDTO;
+import com.myProject.kauan_imovel.infrastructure.mapper.VendaMapper;
+import com.myProject.kauan_imovel.infrastructure.repository.VendaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BuscarTodasVendasHandler {
+    private final VendaRepository vendaRepository;
+    private final VendaMapper vendaMapper;
+
+    public List<VendaQueryDTO> handle() {
+        return vendaRepository.findAll()
+                .stream()
+                .map(vendaMapper::toDTO)
+                .toList();
+    }
+}
