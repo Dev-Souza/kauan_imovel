66a7c79ca81c4a837148a4d7aa4db6582a9df378
Dev-Souza <kauan.souza.dev@gmail.com>
2025-09-01 09:13:42 -0300

    feat: rota de listar compradores =

--- src/main/java/com/myProject/kauan_imovel/api/controller/CompradorController.java
+++ src/main/java/com/myProject/kauan_imovel/api/controller/CompradorController.java
@@ -2,18 +2,25 @@ package com.myProject.kauan_imovel.api.controller;
 
 import com.myProject.kauan_imovel.application.command.handlers.pessoa.CadastrarPessoaCompradoraHandler;
 import com.myProject.kauan_imovel.application.command.pessoa.CadastrarPessoaCompradoraCommand;
+import com.myProject.kauan_imovel.application.query.handler.pessoa.BuscarTodosCompradoresHandler;
+import com.myProject.kauan_imovel.domain.pessoa.dto.PessoaCompradoraQueryDTO;
+import com.myProject.kauan_imovel.domain.pessoa.dto.PessoaProprietarioQueryDTO;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("pessoa-compradora")
+@RequestMapping("/pessoa-compradora")
 @RequiredArgsConstructor
 public class CompradorController {
     private final CadastrarPessoaCompradoraHandler cadastrarPessoaCompradoraHandler;
+    private final BuscarTodosCompradoresHandler buscarTodosCompradoresHandler;
 
-    @PostMapping("cadastrar")
+    @PostMapping("/cadastrar")
     public void cadastrar(@RequestBody CadastrarPessoaCompradoraCommand command) { this.cadastrarPessoaCompradoraHandler.handle(command); }
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<PessoaCompradoraQueryDTO>> listar() {return ResponseEntity.ok().body(this.buscarTodosCompradoresHandler.handle());}
 }
