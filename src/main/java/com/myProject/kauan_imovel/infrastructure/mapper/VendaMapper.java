4ea20efe830519f4edb896cbe53fdc89159ae7b2
Dev-Souza <kauan.souza.dev@gmail.com>
2025-09-01 10:32:12 -0300

    feat: mapper de venda

---
+++ src/main/java/com/myProject/kauan_imovel/infrastructure/mapper/VendaMapper.java
@@ -0,0 +1,46 @@
+package com.myProject.kauan_imovel.infrastructure.mapper;
+
+import com.myProject.kauan_imovel.application.command.venda.CadastrarVendaCommand;
+import com.myProject.kauan_imovel.domain.pessoa.PessoaCompradoraEntity;
+import com.myProject.kauan_imovel.domain.pessoa.PessoaProprietarioEntity;
+import com.myProject.kauan_imovel.domain.pessoa.PessoaVendedoraEntity;
+import com.myProject.kauan_imovel.domain.propriedade.PropriedadeEntity;
+import com.myProject.kauan_imovel.domain.venda.VendaEntity;
+import com.myProject.kauan_imovel.domain.venda.dto.VendaQueryDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface VendaMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "propriedades", ignore = true)
+    @Mapping(target = "vendedor", ignore = true)
+    @Mapping(target = "comprador", ignore = true)
+    @Mapping(target = "proprietario", ignore = true)
+    VendaEntity toEntity(CadastrarVendaCommand command);
+
+    @Mapping(target = "propriedades", expression = "java(mapPropriedadesIds(entity.getPropriedades()))")
+    @Mapping(target = "vendedorId", source = "vendedor.id")
+    @Mapping(target = "compradorId", source = "comprador.id")
+    @Mapping(target = "proprietarioId", source = "proprietario.id")
+    VendaQueryDTO toDTO(VendaEntity entity);
+
+    default VendaEntity toAggregate(
+            CadastrarVendaCommand cmd,
+            PessoaVendedoraEntity vendedor,
+            PessoaCompradoraEntity comprador,
+            PessoaProprietarioEntity proprietario,
+            List<PropriedadeEntity> propriedades
+    ) {
+        VendaEntity venda = toEntity(cmd);
+        venda.setVendedor(vendedor);
+        venda.setComprador(comprador);
+        venda.setProprietario(proprietario);
+        venda.setPropriedades(propriedades);
+        return venda;
+    }
+}
+
