ce86bdc45ec8bc22507d9a44b3c5b60121b63c73
Dev-Souza <kauan.souza.dev@gmail.com>
2025-09-01 11:05:53 -0300

    feat: function de buscar venda por id

--- src/main/java/com/myProject/kauan_imovel/application/query/handler/venda/BuscarVendaPorId.java
+++ src/main/java/com/myProject/kauan_imovel/application/query/handler/venda/BuscarVendaPorId.java
@@ -1,20 +1,16 @@
 package com.myProject.kauan_imovel.application.query.handler.venda;
 
 import com.myProject.kauan_imovel.domain.venda.VendaEntity;
-import com.myProject.kauan_imovel.infrastructure.mapper.VendaMapper;
 import com.myProject.kauan_imovel.infrastructure.repository.VendaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 @RequiredArgsConstructor
-public class BuscarTodasVendasHandler {
+public class BuscarVendaPorId {
     private final VendaRepository vendaRepository;
-    private final VendaMapper vendaMapper;
 
-    public List<VendaEntity> handle() {
-        return vendaRepository.findAll();
+    public VendaEntity handle(Long id) {
+        return vendaRepository.findById(id).orElse(null);
     }
 }
