2b76daea76daed394f2f19fcfd9ef06d448a43c9
Dev-Souza <kauan.souza.dev@gmail.com>
2025-09-02 12:15:45 -0300

    feat: controller de venda

---
+++ src/main/java/com/myProject/kauan_imovel/api/controller/VendaController.java
@@ -0,0 +1,40 @@
+package com.myProject.kauan_imovel.api.controller;
+
+import com.myProject.kauan_imovel.application.command.handlers.venda.AlterarVendaHandler;
+import com.myProject.kauan_imovel.application.command.handlers.venda.CadastrarVendaHandler;
+import com.myProject.kauan_imovel.application.command.handlers.venda.DeletarVendaHandler;
+import com.myProject.kauan_imovel.application.command.venda.CadastrarVendaCommand;
+import com.myProject.kauan_imovel.application.query.handler.venda.BuscarTodasVendasHandler;
+import com.myProject.kauan_imovel.application.query.handler.venda.BuscarVendaPorId;
+import com.myProject.kauan_imovel.domain.venda.dto.VendaQueryDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/venda")
+@RequiredArgsConstructor
+public class VendaController {
+    private final CadastrarVendaHandler cadastrarVendaHandler;
+    private final BuscarTodasVendasHandler buscarTodasVendasHandler;
+    private final BuscarVendaPorId buscarVendaPorId;
+    private final AlterarVendaHandler alterarVendaHandler;
+    private final DeletarVendaHandler deletarVendaHandler;
+
+    @PostMapping("/cadastrar")
+    public void cadastrarVenda(@RequestBody CadastrarVendaCommand command) {this.cadastrarVendaHandler.handle(command);}
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<VendaQueryDTO>> listarVendas() {return ResponseEntity.ok().body(this.buscarTodasVendasHandler.handle());}
+
+    @GetMapping("/listar/{id}")
+    public ResponseEntity<VendaQueryDTO> listarVendaPorId(@PathVariable("id") Long id) {return ResponseEntity.ok().body(this.buscarVendaPorId.handle(id));}
+
+    @PutMapping("/alterar/{id}")
+    public void alterar(@PathVariable("id") Long id, @RequestBody CadastrarVendaCommand command) {this.alterarVendaHandler.handle(id, command);}
+
+    @DeleteMapping("/deletar/{id}")
+    public void deletar(@PathVariable("id") Long id) {this.deletarVendaHandler.handle(id);}
+}
