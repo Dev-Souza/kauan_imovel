ee49adc2a27b222ef2e1c064ce8174861f6f4a99
Dev-Souza <kauan.souza.dev@gmail.com>
2025-09-04 09:42:32 -0300

    feat: funcao de verificar a disponibilidade do imovel

--- src/main/java/com/myProject/kauan_imovel/application/command/handlers/venda/CadastrarVendaHandler.java
+++ src/main/java/com/myProject/kauan_imovel/application/command/handlers/venda/CadastrarVendaHandler.java
@@ -1,11 +1,15 @@
 package com.myProject.kauan_imovel.application.command.handlers.venda;
 
 import com.myProject.kauan_imovel.application.command.venda.CadastrarVendaCommand;
+import com.myProject.kauan_imovel.application.exceptions.ImovelNaoDisponivel;
+import com.myProject.kauan_imovel.domain.propriedade.PropriedadeEntity;
 import com.myProject.kauan_imovel.domain.venda.VendaEntity;
 import com.myProject.kauan_imovel.infrastructure.mapper.VendaMapper;
 import com.myProject.kauan_imovel.infrastructure.repository.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 @RequiredArgsConstructor
@@ -30,6 +34,13 @@ public class CadastrarVendaHandler {
         // Buscando propriedades
         var propriedades = propriedadeRepository.findAllById(command.propriedades());
 
+        for (PropriedadeEntity entity : propriedades) {
+            if(entity.isDisponivel()){
+                propriedadeRepository.alterarDiponibilidadeDoImovel(entity.getId());
+            } else {
+                throw new ImovelNaoDisponivel("Imóvel não esta disponível para venda!");
+            }
+        }
         VendaEntity prop = mapper.toAggregate(command, pessoaVendedor, pessoaCompradora, pessoaProprietario, propriedades);
         // Persistindo
         vendaRepository.save(prop);
--- src/main/java/com/myProject/kauan_imovel/infrastructure/repository/PropriedadeRepository.java
+++ src/main/java/com/myProject/kauan_imovel/infrastructure/repository/PropriedadeRepository.java
@@ -1,7 +1,15 @@
 package com.myProject.kauan_imovel.infrastructure.repository;
 
 import com.myProject.kauan_imovel.domain.propriedade.PropriedadeEntity;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PropriedadeRepository extends JpaRepository<PropriedadeEntity, Long> {
+    @Modifying
+    @Transactional
+    @Query("UPDATE PropriedadeEntity p SET p.disponivel = false WHERE p.id = :idImovel")
+    void alterarDiponibilidadeDoImovel(@Param("idImovel") Long idImovel);
 }
